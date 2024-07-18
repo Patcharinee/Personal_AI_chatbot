dc4e463111554ab6f7ec16c8c17cc1c58c710bbf
Fai <patcharinee@gmail.com>
2024-07-18 18:32:42 +0700

    initial

---
+++ qa.py
@@ -0,0 +1,70 @@
+from langchain_community.vectorstores import Chroma
+from langchain_community.chat_models import ChatOllama
+from langchain_community.embeddings import FastEmbedEmbeddings
+from langchain_community.document_loaders import PyPDFLoader
+from langchain_text_splitters import RecursiveCharacterTextSplitter
+from langchain_community.vectorstores.utils import filter_complex_metadata
+from langchain_core.output_parsers import StrOutputParser
+from langchain_core.prompts import PromptTemplate
+from langchain_core.runnables import RunnablePassthrough
+
+chunk_size = 1024
+chunk_overlap = 100
+llm_model = "llama3" # llama3, mistral, gemma2
+
+class AskMe:
+    vector_store = None
+    retriever = None
+    chain = None
+
+    def __init__(self):
+        self.model = ChatOllama(model=llm_model)
+        self.text_splitter = RecursiveCharacterTextSplitter(chunk_size=chunk_size, chunk_overlap=chunk_overlap)
+        self.prompt = PromptTemplate.from_template(
+            # # rag-prompt-mistral
+            # """
+            # <s> [INST] You are an assistant for question-answering tasks. Use the following pieces of retrieved context to answer the question. If you don't know the answer, just say that you don't know. Use three sentences maximum and keep the answer concise. [/INST] </s> 
+            # [INST] Question: {question} 
+            # Context: {context} 
+            # Answer: [/INST]
+            # """
+
+            # rag-prompt-llama3
+            """
+            <|begin_of_text|><|start_header_id|>system<|end_header_id|> You are an assistant for question-answering tasks. Use the following pieces of retrieved context to answer the question. If you don't know the answer, just say that you don't know. Use three sentences maximum and keep the answer concise <|eot_id|><|start_header_id|>user<|end_header_id|> 
+            Question: {question} 
+            Context: {context} 
+            Answer: <|eot_id|><|start_header_id|>assistant<|end_header_id|>
+            """
+        )
+
+    def embed(self, pdf_file_path: str):
+        docs = PyPDFLoader(file_path=pdf_file_path).load()
+        chunks = self.text_splitter.split_documents(docs)
+        chunks = filter_complex_metadata(chunks)
+
+        vector_store = Chroma.from_documents(documents=chunks, embedding=FastEmbedEmbeddings())
+        self.retriever = vector_store.as_retriever(
+            search_type="similarity_score_threshold",
+            search_kwargs={
+                "k":3,
+                "score_threshold":0.5,
+            },
+        )
+
+        self.chain = ({"context": self.retriever, "question": RunnablePassthrough()}
+                      | self.prompt
+                      | self.model
+                      | StrOutputParser()
+                      )
+
+    def ask(self, query: str):
+        if not self.chain:
+            return "Please add a PDF document first."
+        
+        return self.chain.invoke(query)
+    
+    def clear(self):
+        self.vector_store = None
+        self.retriever = None
+        self.chain = None
